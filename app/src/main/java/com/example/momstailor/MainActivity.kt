b0ebee182efb5346b5ebffe7a0b61dcbefea6e37
khushirani005 <khushirani77098@gmail.com>
2023-11-27 22:50:46 +0530

    new update

--- app/src/main/java/com/example/momstailor/MainActivity.kt
+++ app/src/main/java/com/example/momstailor/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.momstailor
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,7 +11,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.momstailor.ui.theme.MomsTailorTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
